e59d31ed4310dddec512ea98e9496523b287010d
Nekrosis <ryuk.nekro@gmail.com>
2022-04-24 20:20:53 +0300

    refactoring

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static  void main(String[] args) {
+    public static void main(String[] args) {
+        String create = " Create";
+        String dirGame = "/Users/macbookpro/Games";
         StringBuilder stringBuilder = new StringBuilder();
         List<String> listDir = new ArrayList<>();
         listDir.add("temp");
         listDir.add("src");
         listDir.add("res");
         listDir.add("savegames");
-        File dir = new File("/Users/macbookpro/Games");
+        File dir = new File(dirGame);
         if (dir.mkdir()) {
-            stringBuilder.append(dir.getName() + " - dir create");
+            stringBuilder.append(dir.getName() + create);
             stringBuilder.append("\n");
         }
         for (String s : listDir) {
-            File dirs = new File("/Users/macbookpro/Games", s);
+            File dirs = new File(dirGame, s);
             if (dirs.mkdirs()) {
-                stringBuilder.append(dirs.getName() + " - dir create");
+                stringBuilder.append(dirs.getName() + create);
                 stringBuilder.append("\n");
             }
         }
@@ -29,9 +31,9 @@ public class Main {
         resDir.add("vectors");
         resDir.add("icons");
         for (String listRe : resDir) {
-            File dirsRes = new File("/Users/macbookpro/Games/res", listRe);
+            File dirsRes = new File(dirGame + "/res", listRe);
             if (dirsRes.mkdirs()) {
-                stringBuilder.append(dirsRes.getName() + " - dir create");
+                stringBuilder.append(dirsRes.getName() + create);
                 stringBuilder.append("\n");
             }
         }
@@ -42,35 +44,30 @@ public class Main {
         list.add("Main.java");
         list.add("Utils.java");
         for (String src : srcDir) {
-            File dirsSrc = new File("/Users/macbookpro/Games/src", src);
+            File dirsSrc = new File(dirGame + "/src", src);
             if (dirsSrc.mkdirs()) {
-                stringBuilder.append(dirsSrc.getName() + " - dir create");
+                stringBuilder.append(dirsSrc.getName() + create);
                 stringBuilder.append("\n");
             }
         }
-        File file1 = new File("/Users/macbookpro/Games/src/main", "Main.java");
-        File file2 = new File("/Users/macbookpro/Games/src/main", "Utils.java");
-        File file3 = new File("/Users/macbookpro/Games/temp", "text.txt");
+        File file1 = new File(dirGame + "/src/main", "Main.java");
+        File file2 = new File(dirGame + "/src/main", "Utils.java");
+        File file3 = new File(dirGame + "/temp", "text.txt");
+        List<File> files = new ArrayList<>();
+        files.add(file1);
+        files.add(file2);
+        files.add(file3);
         try {
-            if (file1.createNewFile()) {
-                stringBuilder.append(file1.getName() + " - file create");
-                stringBuilder.append("\n");
+            for (File file : files) {
+                if (file.createNewFile()) {
+                    stringBuilder.append(file.getName() + create)
+                            .append("\n");
+                    FileWriter text = new FileWriter(file3);
+                    text.write(String.valueOf(stringBuilder));
+                    text.flush();
+                    text.close();
+                }
             }
-            if (file2.createNewFile()) {
-                stringBuilder.append(file2.getName() + " - file create");
-                stringBuilder.append("\n");
-            }
-            if (file3.createNewFile()) {
-                stringBuilder.append(file3.getName() + " - file create");
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            FileWriter text = new FileWriter(file3);
-            text.write(String.valueOf(stringBuilder));
-            text.flush();
-            text.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
