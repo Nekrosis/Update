c52dbab1d77ba981741e7ee7a2479bb229627078
Nekrosis <ryuk.nekro@gmail.com>
2022-04-24 15:19:27 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static  void main(String[] args) {
         StringBuilder stringBuilder = new StringBuilder();
         List<String> listDir = new ArrayList<>();
         listDir.add("temp");
         listDir.add("src");
         listDir.add("res");
         listDir.add("savegames");
-        File dir = new File("C://Games");
+        File dir = new File("/Users/macbookpro/Games");
         if (dir.mkdir()) {
             stringBuilder.append(dir.getName() + " - dir create");
             stringBuilder.append("\n");
         }
         for (String s : listDir) {
-            File dirs = new File("C://Games", s);
+            File dirs = new File("/Users/macbookpro/Games", s);
             if (dirs.mkdirs()) {
                 stringBuilder.append(dirs.getName() + " - dir create");
                 stringBuilder.append("\n");
@@ -29,7 +29,7 @@ public class Main {
         resDir.add("vectors");
         resDir.add("icons");
         for (String listRe : resDir) {
-            File dirsRes = new File("C://Games/res", listRe);
+            File dirsRes = new File("/Users/macbookpro/Games/res", listRe);
             if (dirsRes.mkdirs()) {
                 stringBuilder.append(dirsRes.getName() + " - dir create");
                 stringBuilder.append("\n");
@@ -42,15 +42,15 @@ public class Main {
         list.add("Main.java");
         list.add("Utils.java");
         for (String src : srcDir) {
-            File dirsSrc = new File("C://Games/src", src);
+            File dirsSrc = new File("/Users/macbookpro/Games/src", src);
             if (dirsSrc.mkdirs()) {
                 stringBuilder.append(dirsSrc.getName() + " - dir create");
                 stringBuilder.append("\n");
             }
         }
-        File file1 = new File("C://Games/src/main", "Main.java");
-        File file2 = new File("C://Games/src/main", "Utils.java");
-        File file3 = new File("C://Games/temp", "text.txt");
+        File file1 = new File("/Users/macbookpro/Games/src/main", "Main.java");
+        File file2 = new File("/Users/macbookpro/Games/src/main", "Utils.java");
+        File file3 = new File("/Users/macbookpro/Games/temp", "text.txt");
         try {
             if (file1.createNewFile()) {
                 stringBuilder.append(file1.getName() + " - file create");
